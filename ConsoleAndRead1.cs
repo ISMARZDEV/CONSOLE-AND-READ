5f6a0857fb9de5efe15768a0782a34c1f813b1f6
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 23:01:50 -0400

    Create ConsoleAndRead1.cs

---
+++ ConsoleAndRead1.cs
@@ -0,0 +1,13 @@
+//1.  Imprimir “Martínez Encarnación” por pantalla”
+
+using System;
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(“Martínez Encarnación"); 
+        }   
+    }
+}
