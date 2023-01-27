c471537bb6a507425fc153001210b2ab6aed1286
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 23:07:49 -0400

    Create ConsoleAndRead5.cs

---
+++ ConsoleAndRead5.cs
@@ -0,0 +1,22 @@
+//4. Capture dos números y muestre por pantalla la sumatoria dichos números.
+
+using System;
+
+namespace Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, resultado;
+
+            Console.WriteLine("Digite su 1er numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite su 2do numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            resultado = num1 + num2;
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, resultado);
+        }
+    }
+}
