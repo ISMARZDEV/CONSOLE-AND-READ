1fec14009fe56d7eb0560bf8dfd379ab68086a91
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 23:02:43 -0400

    Create ConsoleAndRead2.cs

---
+++ ConsoleAndRead2.cs
@@ -0,0 +1,13 @@
+//2.  Mostrar “Ismael Porfirio” por pantalla”
+
+using System;
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ismael Porfirio"); 
+        }   
+    }
+}
